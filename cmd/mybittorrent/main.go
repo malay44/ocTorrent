b7a858d30c625b03a7652fa718154233f05c5c83
malay44 <malay.p1@ahduni.edu.in>
2024-09-02 02:14:52 +0530

    code splitting

--- cmd/mybittorrent/main.go
+++ cmd/mybittorrent/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"io"
 	"os"
 	"strconv"
 	"unicode"
@@ -11,39 +12,83 @@ import (
 // Ensures gofmt doesn't remove the "os" encoding/json import (feel free to remove this!)
 var _ = json.Marshal
 
-func decodeBencode(bencodedString string) (interface{}, error) {
+func decodeBencode(bencodedString string, startIndex int) (interface{}, int, error) {
 	strLen := len(bencodedString)
-	firstByte := bencodedString[0]
-	lastByte := bencodedString[strLen-1]
+	firstByte := bencodedString[startIndex]
 
+	if startIndex == strLen {
+		return nil, startIndex, io.ErrUnexpectedEOF
+	}
+
+	switch {
+	case firstByte == 'i':
+		return decodeInt(bencodedString, startIndex);
+	case unicode.IsDigit(rune(firstByte)):
+		return decodeString(bencodedString, startIndex);
+	default:
+		return nil, startIndex, fmt.Errorf("unexpected value: %q", firstByte)
+	}
+}
+
+func decodeString(bencodedString string, startIndex int) (string, int, error) {
+	strLen := len(bencodedString)
+	firstByte := bencodedString[startIndex]
 
 	if unicode.IsDigit(rune(firstByte)) {
 		var colonIndex int
 
-		for i := 0; i < strLen; i++ {
+		for i := startIndex; i < strLen; i++ {
 			if bencodedString[i] == ':' {
 				colonIndex = i
 				break
 			}
 		}
+		
+		if (colonIndex == 0) {
+			return "", startIndex, fmt.Errorf("bad string, didn't found semicolon")
+		}
 
-		lengthStr := bencodedString[:colonIndex]
+		lengthStr := bencodedString[startIndex:colonIndex]
 
 		length, err := strconv.Atoi(lengthStr)
 		if err != nil {
-			return "", err
+			return "", startIndex, err
 		}
 
-		return bencodedString[colonIndex+1 : colonIndex+1+length], nil
-	} else if firstByte == 'i' && lastByte == 'e' {
-		num, err := strconv.Atoi(bencodedString[1 : strLen-1])
-		if err != nil {
-			return "", err
+		if length > strLen {
+			return "", startIndex, fmt.Errorf("bad string, length is greater than string length")
 		}
-
-		return num, nil
+		str := string([]rune(bencodedString)[colonIndex+1:colonIndex+1+length])
+		return str, colonIndex + 1 + length, nil
 	} else {
-		return "", fmt.Errorf("only strings are supported at the moment")
+		return "", startIndex, fmt.Errorf("bad string")
+	}
+}
+
+func decodeInt(bencodedString string, startIndex int) (int, int, error) {
+	strLen := len(bencodedString)
+	firstByte := bencodedString[startIndex]
+
+	if firstByte == 'i' {
+		var endIndex int
+
+		for i := startIndex+1; i < strLen; i++ {
+			if bencodedString[i] == 'e' {
+				endIndex = i
+				break
+			}
+		}
+
+		numStr := bencodedString[startIndex+1:endIndex]
+
+		num, err := strconv.Atoi(numStr)
+		if err != nil {
+			return 0, startIndex, err
+		}
+
+		return num, endIndex+1, nil
+	} else {
+		return 0, startIndex, fmt.Errorf("bad int")
 	}
 }
 
@@ -53,7 +98,7 @@ func main() {
 	if command == "decode" {
 		bencodedValue := os.Args[2]
 		
-		decoded, err := decodeBencode(bencodedValue)
+		decoded,_, err := decodeBencode(bencodedValue, 0)
 		if err != nil {
 			fmt.Println(err)
 			return
